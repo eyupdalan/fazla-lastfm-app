d2d1efacc9a9249564c2cecda841375ea3e728f8
eyupdalan <eyupdalan@gmail.com>
2022-07-17 19:53:50 +0300

    eyup adds artists page developments

--- components/Artist/index.js
+++ components/Artist/index.js
@@ -1,3 +1,13 @@
-export default function Artist({}){
-    return <div>Artist component</div>
+import styles from "../../styles/Artist.module.css";
+
+export default function Artist({mbid, name, image, listeners, playcount}) {
+    return (
+        <div className={styles.artist}>
+            <div>{mbid}</div>
+            <div>{name}</div>
+            <div>{image}</div>
+            <div>{listeners}</div>
+            <div>{playcount}</div>
+        </div>
+    )
 }
--- components/ArtistList/index.js
+++ components/ArtistList/index.js
@@ -2,10 +2,19 @@ import PropTypes from "prop-types";
 import Artist from "../Artist";
 
 export default function ArtistList({data}){
+    const artists = data.map(artist => (
+        <Artist
+            key={artist.mbid}
+            mbid={artist.mbid}
+            name={artist.name}
+            playcount={artist.playcount}
+            listeners={artist.listeners}
+            image={artist.image[0]["#text"]}
+        />
+    ))
     return (
         <div>
-            <div>{JSON.stringify(data)}</div>
-            <Artist/>
+            {artists}
         </div>
     )
 }
