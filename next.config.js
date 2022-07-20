84b05f5be806babae01c9518a79193bc7bd19a25
eyupdalan <eyupdalan@gmail.com>
2022-07-21 01:06:27 +0300

    eyup adds storybook changes

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     images: {
+        unoptimized: true,
         domains: [
             "lastfm.freetls.fastly.net",
             "en.wikipedia.org",
