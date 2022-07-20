8e1ca0def8b139c70f9f13eaddaffcd8c2056773
eyupdalan <eyupdalan@gmail.com>
2022-07-21 01:54:44 +0300

    eyup adds storybook changes

--- next.config.js
+++ next.config.js
@@ -9,6 +9,11 @@ const nextConfig = {
             "upload.wikimedia.org",
             "localhost"
         ]
+    },
+    experimental: {
+        images: {
+            unoptimized: true
+        }
     }
 }
 
