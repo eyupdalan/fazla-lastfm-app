188da6188c0439f538f32df38efc2c311d171c43
eyupdalan <eyupdalan@gmail.com>
2022-07-21 03:31:29 +0300

    eyup adds theme selection

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,26 @@
 import '../styles/globals.css';
 import {QueryClient, QueryClientProvider,} from 'react-query'
-import { ReactQueryDevtools } from 'react-query/devtools'
+import {ReactQueryDevtools} from 'react-query/devtools'
+import {useState} from "react";
 
 const queryClient = new QueryClient()
 
 function MyApp({Component, pageProps}) {
+    const [theme, setTheme] = useState("white");
     return (
         <QueryClientProvider client={queryClient}>
-            <Component {...pageProps} />
-            <ReactQueryDevtools initialIsOpen={false} />
+            <div className={theme}>
+                <div>
+                    white mode: <input type={"radio"} value={"white"} checked={theme === "white"}
+                                       onChange={() => setTheme("white")}/>
+                </div>
+                <div>
+                    dark mode: <input type={"radio"} value={"dark"} checked={theme === "dark"}
+                                      onChange={() => setTheme("dark")}/>
+                </div>
+                <Component {...pageProps}/>
+            </div>
+            <ReactQueryDevtools initialIsOpen={false}/>
         </QueryClientProvider>
     )
 }
